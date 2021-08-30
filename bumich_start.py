09a6434ee2802a3cfbfe1b43a8fef5ef0c51751b
Максим <43538054+EnergikO@users.noreply.github.com>
2021-08-30 20:36:46 +0300

    V 0.65
    Перенёс данные из main

--- bumich_start.py
+++ bumich_start.py
@@ -17,65 +17,71 @@ async def on_ready():
 @client.command()
 # Удаляет столько сообщений, сколько попросят.
 # На пользователей, не имеющих статуса админа/модератора наложены ограничения
-async def clear(ctx, count=1):
+async def clear(ctx, count=None):
     # Создаём видимость работы
     await ctx.trigger_typing()
+
+    # Проверка, ввели ли числа или нет
+    try:
+        count = int(count)
+
+    except ValueError:
+        await ctx.send('Некорректное кол-во')
+        return
 
     # Сколько сообщений можно удалять НЕ админам/модераторам
     max_delete_not_admin = 10
 
-    # Проверка, ввели ли числа или нет
-    if isinstance(count, int):
-        user_top_role = ctx.message.author.top_role
+    user_top_role = ctx.message.author.top_role
 
-        # Только этим ролям доступно удаление без ограничений
-        if str(user_top_role) == "Admin" or str(user_top_role) == "Moderator":
-            await ctx.message.channel.purge(limit=count + 1)
+    # Только этим ролям доступно удаление без ограничений
+    if str(user_top_role) == "Admin" or str(user_top_role) == "Moderator":
+        await ctx.message.channel.purge(limit=count + 1)
 
-        else:
-            # Если малое кол-во, то можно удалить
-            if count <= max_delete_not_admin:
-
-                # Чтобы кто-то не удалил всё за 1500 сообщений в секунду
-                global time_last_purge
-
-                if time() - time_last_purge >= time_for_purge:
-                    # Обновление счёта времени
-                    time_last_purge = time()
-                    # Само удаление сообщений
-                    await ctx.message.channel.purge(limit=count + 1)
-                else:
-                    await ctx.message.send('Прошло недостаточно времени')
-            else:
-                await ctx.message.send(f'Нет права на удаления такого количества сообщений, разрешено удалять '
-                                       f'{max_delete_not_admin} сообщений за раз')
     else:
-        await ctx.message.send('Некорректное число')
+        # Если малое кол-во, то можно удалить
+        if count <= max_delete_not_admin:
+
+            # Чтобы кто-то не удалил всё за 1500 сообщений в секунду
+            global time_last_purge
+
+            if time() - time_last_purge >= time_for_purge:
+                # Обновление счёта времени
+                time_last_purge = time()
+                # Само удаление сообщений
+                await ctx.message.channel.purge(limit=count + 1)
+            else:
+                await ctx.send('Прошло недостаточно времени')
+        else:
+            await ctx.send(f'Нет права на удаления такого количества сообщений, разрешено удалять '
+                           f'{max_delete_not_admin} сообщений за раз')
 
 
 @client.command()
 # Пишет в чат набор из случайных подбрасываний монетки, нет ограничений
-async def coin(ctx, count=1):
+async def coin(ctx, count=None):
     # Создаём видимость работы
     await ctx.trigger_typing()
 
     # Проверка, ввели ли числа или нет
-    if isinstance(count, int):
+    try:
+        count = int(count)
 
-        answer = ''
-        for i in range(count):
-            temp_coin = randint(0, 1)
-
-            if temp_coin == 1:
-                answer += 'Орёл '
-            else:
-                answer += 'Решка '
-
-        # Вывод
-        await ctx.send(answer)
-
-    else:
+    except ValueError:
         await ctx.send('Некорректное кол-во')
+        return
+
+    answer = ''
+    for i in range(count):
+        temp_coin = randint(0, 1)
+
+        if temp_coin == 1:
+            answer += 'Орёл '
+        else:
+            answer += 'Решка '
+
+    # Вывод
+    await ctx.send(answer)
 
 
 @client.command()
@@ -136,36 +142,38 @@ async def commands(ctx, command_name=None):
 
 @client.command()
 # Пишет в чат набор из случайных чисел, нет ограничений, ввод диапазона чисел в любом порядке
-async def roll(ctx, roll_first_number=100, roll_second_number=1, count=1):
+async def roll(ctx, roll_first_number=None, roll_second_number=None, count=None):
     # Создаём видимость работы
     await ctx.trigger_typing()
 
-    # Проверка, ввели ли числа или нет
-    if isinstance(roll_first_number, int) and isinstance(roll_second_number, int) and isinstance(count, int):
+    # Проверка, что ввели, а что нет
+    # То, что не ввели или ввели неверно значения возьмутся по умолчанию
 
-        if count <= 0:
-            await ctx.send('Некорректное кол-во')
+    if may_be_class(roll_first_number):  roll_first_number = int(roll_first_number)
+    else:  roll_first_number = 100
 
-        else:
-            # Проверка и замена для стабильной работы рандома
-            if count > roll_second_number:
-                roll_first_number, roll_second_number = roll_second_number, roll_first_number
+    if may_be_class(roll_second_number):  roll_second_number = int(roll_second_number)
+    else:  roll_second_number = 1
 
-            # Проверка на правильность выходного предложения
-            if count == 1:
-                temp_str = f'Случайное число от {roll_first_number} до {roll_second_number}:\n'
-            else:
-                temp_str = f'Случайные числа от {roll_first_number} до {roll_second_number}:\n'
+    if may_be_class(count): count = abs(int(count))
+    else:  count = 1
 
-            # Вычисления случайных чисел
-            for i in range(count):
-                temp_str += str(randint(roll_first_number, roll_second_number)) + ' '
+    # Проверка и замена для стабильной работы рандома
+    if roll_first_number > roll_second_number:
+        roll_first_number, roll_second_number = roll_second_number, roll_first_number
 
-            # Вывод
-            await ctx.send(temp_str)
-
+    # Проверка на правильность выходного предложения
+    if count == 1:
+        temp_str = f'Случайное число от {roll_first_number} до {roll_second_number}:\n'
     else:
-        await ctx.send('Вы ввели не числа')
+        temp_str = f'Случайные числа от {roll_first_number} до {roll_second_number} (их тут {count}):\n'
+
+    # Вычисления случайных чисел
+    for i in range(count):
+        temp_str += str(randint(roll_first_number, roll_second_number)) + ' '
+
+    # Вывод
+    await ctx.send(temp_str)
 
 
 @client.command()
@@ -176,6 +184,18 @@ async def stop(ctx):
         exit()
     else:
         await ctx.send('Ты не мой братик, тебе нельзя мной командовать.')
+
+
+def may_be_class(value, class_name=int) -> bool:
+    if value is None:
+        return False
+
+    try:
+        value = class_name(value)
+        return True
+
+    except ValueError:
+        return False
 
 
 client.run(settings["token"])
