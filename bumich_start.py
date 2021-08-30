6b8bd33954a454e8a52765bb68bfb049ed05d112
Trigger <energiko71@gmail.com>
2021-08-30 17:11:44 +0300

    v 0.5
    начало совместной разработки

---
+++ bumich_start.py
@@ -0,0 +1,177 @@
+from random import randint
+from time import time
+from discord.ext import commands
+from settings import settings
+
+client = commands.Bot(command_prefix=settings['prefix'])
+
+time_for_purge = 15  # Удаления сообщений. НЕ админы могут только раз в столько секунд
+time_last_purge = 0  # Сколько секунд назад последний раз удаляли сообщения
+
+
+@client.event
+async def on_ready():
+    print(f"{settings['botName']} successful launch!")
+
+
+@client.command()
+# Удаляет столько сообщений, сколько попросят.
+# На пользователей, не имеющих статуса админа/модератора наложены ограничения
+async def clear(ctx, count=1):
+    # Создаём видимость работы
+    await ctx.trigger_typing()
+
+    # Проверка, ввели ли числа или нет
+    if isinstance(count, int):
+        user_top_role = ctx.message.author.top_role
+
+        # Только этим ролям доступно удаление без ограничений
+        if str(user_top_role) == "Admin" or str(user_top_role) == "Moderator":
+            await ctx.message.channel.purge(limit=count + 1)
+
+        else:
+            # Если малое кол-во, то можно удалить
+            if count <= 5:
+
+                # Чтобы кто-то не удалил всё за 1500 сообщений в секунду
+                global time_last_purge
+
+                if time() - time_last_purge >= time_for_purge:
+                    # Обновление счёта времени
+                    time_last_purge = time()
+                    # Само удаление сообщений
+                    await ctx.message.channel.purge(limit=count + 1)
+                else:
+                    await ctx.message.send('Прошло недостаточно времени')
+            else:
+                await ctx.message.send('Нет права на удаления такого количества сообщений')
+    else:
+        await ctx.message.send('Некорректное число')
+
+
+@client.command()
+# Пишет в чат набор из случайных подбрасываний монетки, нет ограничений
+async def coin(ctx, count=1):
+    # Создаём видимость работы
+    await ctx.trigger_typing()
+
+    # Проверка, ввели ли числа или нет
+    if isinstance(count, int):
+
+        answer = ''
+        for i in range(count):
+            temp_coin = randint(0, 1)
+
+            if temp_coin == 1:
+                answer += 'Орёл '
+            else:
+                answer += 'Решка '
+
+        # Вывод
+        await ctx.send(answer)
+
+    else:
+        await ctx.send('Некорректное кол-во')
+
+
+@client.command()
+# Выводит инструкции, по заданной команде
+async def commands(ctx, command_name=None):
+    # Создаём видимость работы
+    await ctx.trigger_typing()
+
+    commands_name = ['clear', 'coin', 'commands / help', 'roll', 'stop']
+
+    # Лист всех команд
+    if command_name is None:
+        what_send = '```Вот, что я могу:\n'
+        for command in commands_name:
+            what_send += command + '\n'
+        what_send += '```'
+
+        await ctx.send(what_send)
+
+    elif command_name == 'clear':
+        await ctx.send(
+            '.clear <количество_сообщений>\n\n' +
+
+            'Удаляет столько сообщений, сколько попросят.\n' +
+            'На пользователей, не имеющих статуса админа/модератора наложены ограничения\n\n'
+
+            'Временно не работает в ЛС с ботом')
+    elif command_name == 'coin':
+        await ctx.send(
+            '.coin <количество_бросков>\n\n' +
+
+            'Пишет в чат набор из случайных подбрасываний монетки, нет ограничений\n'
+        )
+    elif command_name in 'commands / help':
+        await ctx.send(
+            '.commands <команда>\n\n' +
+
+            'Выводит инструкции по заданной команде'
+        )
+    elif command_name == 'roll':
+        await ctx.send(
+            '.roll <граница 1> <граница 2> <количество чисел>\n' +
+            'Значения по умолчанию:\n'
+            '<граница 1> - 100\n<граница 2> - 1\n'
+            '<количество чисел> - 1\n\n'
+
+            'Пишет в чат набор из случайных чисел, нет ограничений, ввод диапазона чисел в любом порядке'
+        )
+    elif command_name == 'stop':
+        await ctx.send(
+            'Иди своей дорогой сталкер'
+        )
+    else:
+        await ctx.send(
+            'Такой команды нет'
+        )
+
+
+@client.command()
+# Пишет в чат набор из случайных чисел, нет ограничений, ввод диапазона чисел в любом порядке
+async def roll(ctx, roll_first_number=100, roll_second_number=1, count=1):
+    # Создаём видимость работы
+    await ctx.trigger_typing()
+
+    # Проверка, ввели ли числа или нет
+    if isinstance(roll_first_number, int) and isinstance(roll_second_number, int) and isinstance(count, int):
+
+        if count <= 0:
+            await ctx.send('Некорректное кол-во')
+
+        else:
+            # Проверка и замена для стабильной работы рандома
+            if count > roll_second_number:
+                roll_first_number, roll_second_number = roll_second_number, roll_first_number
+
+            # Проверка на правильность выходного предложения
+            if count == 1:
+                temp_str = f'Случайное число от {roll_first_number} до {roll_second_number}:\n'
+            else:
+                temp_str = f'Случайные числа от {roll_first_number} до {roll_second_number}:\n'
+
+            # Вычисления случайных чисел
+            for i in range(count):
+                temp_str += str(randint(roll_first_number, roll_second_number)) + ' '
+
+            # Вывод
+            await ctx.send(temp_str)
+
+    else:
+        await ctx.send('Вы ввели не числа')
+
+
+@client.command()
+# Иди своей дорогой сталкер
+async def stop(ctx):
+    if ctx.message.author.id == 401105274955497482:
+        await ctx.send('Ну, я пошёл...')
+        exit()
+    else:
+        await ctx.send('Ты не мой братик, тебе нельзя мной командовать.')
+
+
+client.run(settings["token"])
